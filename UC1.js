fecc0a0657a84a4c12df19739180113b6d1d8dd3
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-03-24 19:10:04 +0530

    Calculate the Employee Wage Based on part time or full time work using Switch Case

--- UC1.js
+++ UC1.js
@@ -1,11 +1,23 @@
 {
-	const IS_ABSENT = 0;
+	const IS_PART_TIME = 1;
+	const IS_FULL_TIME = 2;
+	const PART_TIME_HOURS = 4;
+	const FULL_TME_HOURS = 8;
+	const WAGE_PER_HOUR = 20;
 
-	let employeeCheck = Math.floor(Math.random() * 10 % 2);
-	if (employeeCheck == IS_ABSENT) {
-		console.log("Employee is Absent");
-	} else {
-		console.log("Employee is Present");
+	let empHours = 0;
+	let employeeCheck = Math.floor(Math.random() * 10 % 3);
+	switch (employeeCheck) {
+		case IS_PART_TIME:
+			empHours = PART_TIME_HOURS;
+			break;
+		case IS_FULL_TIME:
+			empHours = FULL_TME_HOURS;
+			break;
+		default:
+			empHours = 0;
 	}
-    
+
+	let empWage = empHours * WAGE_PER_HOUR;
+	console.log('Employee Wage : ' + empWage);
 }
